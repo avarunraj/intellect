e4fb554974a156eea7e512f888f99fc5c33cfe9e
avarunraj <arunraja857@gmail.com>
2023-01-21 21:59:24 +0530

    removed app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import "./App.css";
 import { CircularProgressBar } from "./Components/CircularProgressBar";
 import { TriangleStatusBar } from "./Components/TriangleStatusBar";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <CircularProgressBar svgSize={200} progressCircleBarWidth={5} />
       <TriangleStatusBar widgetSize={200} />
-    </div>
+    </>
   );
 }
 
